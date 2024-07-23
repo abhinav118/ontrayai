243f44fc51c2678aef6dffb4b36783992eb83bd6
Abhi <abhinav118@gmail.com>
2024-07-23 10:51:43 -0400

    Update server.js for OPENAI_API_KEY

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors()); // Add this line to enable CORS
 app.use(bodyParser.json());
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_AI_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
